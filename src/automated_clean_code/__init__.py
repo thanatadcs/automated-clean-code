44e05dca27a26a43eb1a71ac19046238eda01280
thanatadcs <thanatad.cs@gmail.com>
2023-01-27 15:41:15 +0700

    Added subtract function and test

--- src/automated_clean_code/__init__.py
+++ src/automated_clean_code/__init__.py
@@ -9,3 +9,16 @@ def add_numbers(x: int, y: int) -> int:
       (int). The sum of the two numbers.
     """
     return x + y
+
+
+def subtract_numbers(x: int, y: int) -> int:
+    """Add two numbers together.
+
+    Args:a
+      x (int): a number
+      y (int): a number
+
+    Returns:
+      (int). The difference of the two numbers.
+    """
+    return x - y
--- tests/test_add.py
+++ tests/test_add.py
@@ -7,3 +7,15 @@ def test_sanity():
 
 def test_add():
     assert automated_clean_code.add_numbers(1, 2) == 3
+
+
+"""
+Just automated_clean_code.subtract_numbers(1, 1) without assert is cheating
+line coverage is a necessary condition, but not sufficient
+you need assert too
+Another way to cheat is "git commit -n"
+"""
+
+
+def test_subtract():
+    assert automated_clean_code.subtract_numbers(1, 1) == 0  # pragma: no cover
